17b3c2ab4dfd84197c70ac16bcd324240d3dec6e
Miguell02 <miguelgonzalez1@csumb.edu>
2025-02-11 14:42:56 -0800

    Initial test push for myTeam branch

---
+++ new-app/app/(tabs)/myTeam.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Test Component</h1>
+      <p>This is a test push to verify the myTeam branch.</p>
+    </div>
+  );
+};
+
+export default TestComponent;
